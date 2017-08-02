3a11cc101964c0c30058bd6619abbee42642f64b
cycorld <cycorld.com@gmail.com>
2017-08-02 13:57:01 +0900

    spot에 맞게 라우트 수정

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
+         #:confirmable
   validates :name, presence: true
   validates :phone, presence: true
   has_many :spots
